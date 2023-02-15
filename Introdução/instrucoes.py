2d6631705d050341323d8c325b98158afa81dace
Wesley Constantino <99278991+WesleyConstantino@users.noreply.github.com>
2023-02-15 15:49:02 -0300

    Update instrucoes.py

--- Introdução/instrucoes.py
+++ Introdução/instrucoes.py
@@ -7,3 +7,23 @@
 
 #Instalação e configuração do PyGame:
 #https://www.youtube.com/watch?v=wAI0ee2d5jk
+
+
+####################################################################################
+#                           Instruções de comandos
+####################################################################################
+
+#importa a biblioteca do pygame
+import pygame
+
+#inicia os móduloas dessa biblioteca
+pygame.init()
+
+#seta o tamanho da janela, aqui de 600 por 400
+screen = pygame.display.set_mode((600,400))
+
+#troca a cor da janela de fundo para branco
+screen.fill((255,255,255))
+
+#atualiza a cor da janela
+pygame.display.flip()
