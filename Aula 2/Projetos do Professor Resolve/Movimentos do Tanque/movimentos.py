2a9b61b35aa3ec1dde120f31601fdf611b77447c
Wesley Constantino <99278991+WesleyConstantino@users.noreply.github.com>
2023-02-15 16:38:05 -0300

    Update movimentos.py

--- Aula 2/Projetos do Professor Resolve/Movimentos do Tanque/movimentos.py
+++ Aula 2/Projetos do Professor Resolve/Movimentos do Tanque/movimentos.py
@@ -1,32 +1,47 @@
+#Movimentando o Tanque
+
 import pygame
+import math
 from pygame.locals import *
 from sys import exit
 
+
+def movimento(superficie,anglulo):
+    move_x, move_y = pygame.mouse.get_pos()
+    rel_x, rel_y = move_x - x, move_y - y
+    print(rel_x, rel_y)
+    anglulo = (720 / math.pi) * -math.atan2(rel_y, rel_x)
+    rotated_surface = pygame.transform.movimento(superficie,int(anglulo))
+    rotated_rect = rotated_surface.get_rect(center=(300,300))
+    return rotated_surface,rotated_rect
+
+#inicia os móduloas dessa biblioteca
 pygame.init()
-SCREEN_SIZE =(800,600)
-screen = pygame.display.set_mode(SCREEN_SIZE, 0 ,32)
 
-tank = pygame.image.load('tanque.jpg').convert()
+#seta o tamanho da janela
+screen = pygame.display.set_mode([600,600])
+tank = pygame.image.load('toptank.png')
 
-x,y=0,0
-move_x, move_y = 0,0
+anglulo = 0
+
+x=0
+y=0
+move_x = 1
+move_y = 1
 
 while True:
+
+    #Fecha o jogo
     for event in pygame.event.get():
-        if event.type == QUIT:
+        if event.type == pygame.QUIT:
             pygame.quit()
-            exit()
-        if event.type == KEYDOWN:
-            if event.key==K_LEFT:
-                move_x=-1
-        if event.type == KEYUP:
-            if event.key == K_LEFT:
-                move_x=0
+            quit()
 
-        x += move_x
-        y += move_y
-
-        screen.fill((255,255,255))
-        screen.blit(tank,(x,y))
-
-        pygame.display.update()
+    #muda a cor da tela para branco     
+    screen.fill((255,255,255))
+    
+    tank_rotated, tank_rotated_rect = movimento(tank,anglulo)
+    screen.blit(tank_rotated,tank_rotated_rect)
+    
+    #atualiza a cor da janela
+    pygame.display.flip()
