b58073574008f077aba5a2afa6882f7b254700f9
Wesley Constantino <99278991+WesleyConstantino@users.noreply.github.com>
2023-02-15 15:42:15 -0300

    Create instrucoes.py

---
+++ Introdução/instrucoes.py
@@ -0,0 +1,9 @@
+#Todas essa instruções se encontram no primeiro vídeo do Professor Resolve da Aula 2
+
+#Recomenda-se intalar o Python versão 3.4.0
+
+#Download do Python:
+#http://pygame.org/download.shtml
+
+#Instalação e configuração do PyGame:
+#https://www.youtube.com/watch?v=wAI0ee2d5jk
